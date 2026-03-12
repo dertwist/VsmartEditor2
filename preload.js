5c5ed65d04f367abc1844f02af74fc5d2fdf419f
dertwist <45437750+dertwist@users.noreply.github.com>
2026-03-12 19:51:34 +0100

    Update project files from ZIP

--- preload.js
+++ preload.js
@@ -1,6 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  onOpenFile: (callback) => ipcRenderer.on('open-file', (_event, path) => callback(path)),
-  readFile: (filePath) => ipcRenderer.invoke('read-file', filePath)
+onOpenFile: (callback) => ipcRenderer.on('open-file', (_event, path) => callback(path)),
+readFile: (filePath) => ipcRenderer.invoke('read-file', filePath)
 })
